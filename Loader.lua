b1224d3375ec01b0f8dad2e1a8dac3f36abd5283
SirenX-alt <sirenxhub@gmail.com>
2026-01-03 01:56:41 +0700

    Add files via upload

---
+++ Loader.lua
@@ -0,0 +1,20 @@
+local placeIds = {
+    [76558904092080] = "649ea55511e8302dc8a59efb1af6b9dc", -- forge 1
+    [129009554587176] = "649ea55511e8302dc8a59efb1af6b9dc", -- forge 2
+    [131884594917121] = ''649ea55511e8302dc8a59efb1af6b9dc'',
+}
+
+local currentPlaceId = game.PlaceId
+local scriptId = placeIds[currentPlaceId]
+
+if scriptId then
+    local success, result = pcall(function()
+        return loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/" .. scriptId .. ".lua"))()
+    end)
+    
+    if not success then
+        warn("Load failed:", result)
+    end
+else
+    warn("Game not supported. PlaceId:", currentPlaceId)
+end
